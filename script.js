8cb312ca6caf4a9464d7f4b4f3cccdf454c2074a
Vanessa <vanessafranzotti@gmail.com>
2022-10-11 13:48:44 -0300

    Finalizando entrega

--- script.js
+++ script.js
@@ -1,26 +1,54 @@
-let valorDaCompra = parseFloat(prompt("Digite o valor que deseja abastecer:"))
+/*let valorDaCompra = parseFloat(prompt("Digite o valor que deseja abastecer:"))
 let valorLitro = 6.80
 let litros = valorDaCompra / valorLitro
 alert(`O cliente abasteceu ${litros}`)
 
 
-let tempCelsius = parseFloat(prompt("Digite a temperatura em Celsius: "))
+/*let tempCelsius = parseFloat(prompt("Digite a temperatura em Celsius: "))
 let tempFahrenheit = (9 * tempCelsius + 160) /5
 alert(`O valor ${tempCelsius} em Fahrenheit é ${tempFahrenheit} `)
 
 
-let temp_fahrenheit = parseFloat(prompt("Digite a temperatura em Fahrenheit:"))
+/*let temp_fahrenheit = parseFloat(prompt("Digite a temperatura em Fahrenheit:"))
 let temp_celsius = ((temp_fahrenheit -32) *5)/9
 alert(`O valor ${temp_fahrenheit} em Celsius é ${temp_celsius}`)
 
-let comprimento = prompt("Digite o comprimento da caixa:")
+/*let comprimento = prompt("Digite o comprimento da caixa:")
 let largura = prompt("Digite a largura da caixa:")
 let altura = prompt ("Digite a altura da caixa: ")
 let volume = comprimento * largura * altura
 alert(`Área da caixa é: ${volume} m²`)
 
-let numero = parseInt(prompt("Digite um número :"))
+/*let numero = parseInt(prompt("Digite um número :"))
 let valorQuadrado = numero ** 2
-alert(`O valor ${numero} ao quadrado é ${valorQuadrado} `)
+alert(`O valor ${numero} ao quadrado é ${valorQuadrado} `)*/
+
+/*let n1 = parseInt(prompt("Digite a primeira nota:"))
+let n2 = parseInt(prompt("Digite a segunda nota:"))
+let n3 = parseInt(prompt("Digite a terceira nota:"))
+let n4 = parseInt(prompt("Digite a quarta nota:"))
+let n5 = parseInt(prompt("Digite a quinta nota:")) 
+
+let somaTotal = n1 +n2 +n3 +n4 +n5
+let media = somaTotal / 5
+alert(`A soma dos valores informados é: ${somaTotal} e a média das notas é: ${media}`);*/
+
+/*let valor1 = parseInt(prompt("Digite um número positivo:"))
+let valor2 = parseInt(prompt("Digite outro número positivo:"))
+let resto = (valor1 % valor2)
+alert(`O resto da divisão de ${valor1} por ${valor2} é: ${resto}`)*/
+
+/*let frase = prompt("Digite uma palavra ou frase:")
+alert(`Este texto tem ${frase.length} caracteres`)*/
+
+/*let frase1 = prompt("Digite uma frase ou palavra:")
+alert(`A primeira letra deste texto é ${[frase1 [0]]} e a ultima é ${frase1[frase1.length-1]}`)*/
+
+/*let nome = prompt("Digite seu nome: ");
+let sobrenome = prompt("Digite seu sobrenome: ");
+let idade = prompt("Digite sua idade: ")
+let setor = prompt("Digite seu setor: ")
+
+alert(`Nome:${nome} ${sobrenome} - idade ${idade} -Setor: ${setor}`)*/
 
 
